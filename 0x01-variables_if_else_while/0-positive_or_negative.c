a3102ea8fa2e9eb853d2105106fdfd8c31a8114b
Kelebohile53 <phaduanna@gmail.com>
2022-09-09 18:15:58 -0700

    POES

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,9 @@
 
 /**
 * main - print if the number is positive, zero, or negative
-* Description: using the main function
-* this program prints "Programmimg is positive,zero, or negative
+*
+* Desccription: using the main function
+* this program prints "Programming is positive, zero, or negative"
 * Return: 0
 */
 int main(void)
@@ -13,26 +14,18 @@ int main(void)
 int n;
 
 srand(time(0));
-n = rand() - RAND_MAX / 2;
+n = rand() - RAND_Max / 2;
   /* your code goes there */
 if (n > 0)
 {
-
-
-
-
-
-
-
-
-
-          printf("%i is positive\n",n);
+        printf("%i is positive\n", n);
 }
-elseif (n ==0)
+else if (n == 0)
 {
-        printf("%i is zero\n", n);
+        printf("%i is zero\n" , n);
 }
-else if (n < o)
-{
-        print ("% is negative\n", n);
+else if (n < 0)
+        printf{"%i is negative\n", n);
+}
 return (0);
+}
