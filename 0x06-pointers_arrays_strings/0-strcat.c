3f3a0e4f3d1da0f43a2a474bfaddb349dfb14540
Kelebohile53 <phaduanna@gmail.com>
2022-09-22 01:47:40 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,44 +1,24 @@
-#ifndef MAIN_H                                                                                                                                       
+#include "main.h"
 
-#define MAIN_H                                                                                                                                       
+/**
+* _strcat - concatenates the string pointed to by @src to
+*the end of the string pointed to by @dest
+*@dest: String that will be appended
+*@src: String to be concatenated upon
+*
+*Return: returns pointer to @dest
+*/
 
-                                                                                                                                                     
+char *_strcat(char *dest, char *src)
+{
 
-#include <stdio.h>                                                                                                                                   
+int index = 0, dest_len = 0;
 
-                                                                                                                                                     
+	while(dest[index++])
+		dest_len++;
 
-/**                                                                                                                                                  
+	for(index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-*void prototypes()                                                                                                                                   
-
-*int prototypes()                                                                                                                                    
-
-*char prototypes()                                                                                                                                   
-
-*/                                                                                                                                                   
-
-void reverse_array(int *a, int n);                                                                                                                   
-
-void print_number(int n);                                                                                                                            
-
-void print_buffer(char *b, int size);                                                                                                                
-
-int _strcmp(char *s1, char *s2);                                                                                                                     
-
-char *_strcat(char *dest, char *src);                                                                                                                
-
-char *_strncat(char *dest, char *src, int n);                                                                                                        
-
-char *_strncpy(char *dest, char *src, int n);                                                                                                        
-
-char *string_toupper(char *);                                                                                                                        
-
-char *cap_string(char *);                                                                                                                            
-
-char *leet(char *);                                                                                                                                  
-
-char *rot13(char *);                                                                                                                                 
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);                                                                                         
-
+	return (dest);
+}
