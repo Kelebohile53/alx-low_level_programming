36f66f52ce688a59f2e33f07efbb28e3c86b1aa5
Kelebohile53 <phaduanna@gmail.com>
2022-12-16 19:53:47 -0800

    0-isupper.cc

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,37 +1,22 @@
 #include "main.h"
 
-/**
- *
- *  * _isupper - function for upper case
- *
- *   *
- *
- *    * @c: holder for the arguments passed
- *
- *     *
- *
- *      * Return: 1 0r 0
- *
- *       */
+/*
+ * _isupper - function for upper case
+ * @c: holder for the arguments passed
+ * Return: 1 0r 0
+ */
 
 int _isupper(int c)
 
 {
 
-		if (c >= 'A' && c <= 'Z')
-
-				{
-
-							return (1);
-
-								}
-
-			else
-
-					{
-
-								return (0);
-
-									}
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 
 }
